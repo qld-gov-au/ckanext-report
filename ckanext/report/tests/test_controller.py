596c22def57754758f951621b9f87dc2ef696fe6
ThrawnCA <shell_layer-github@yahoo.com.au>
2021-11-03 15:54:33 +1000

    [QOL-7906] extract helper function to get response body
    - adapt to different response object types for different CKAN versions

--- ckanext/report/tests/test_controller.py
+++ ckanext/report/tests/test_controller.py
@@ -11,12 +11,23 @@ def _setup_function(self):
     self.app = helpers._get_test_app()
 
 
+def _get_response_body(response):
+    ''' Extract the response body of a Webtest or Flask response as text.
+    '''
+    if hasattr(response, 'html'):
+        return response.html.string
+    elif hasattr(response, 'get_data'):
+        return response.get_data(as_text=True)
+    else:
+        raise Exception("Unrecognised response object: [{}]".format(response))
+
+
 @with_setup(_setup_function)
 class TestController():
 
     def test_report_index(self):
         response = self.app.get('/report', status=200)
-        assert_in('Reports', response.html.head.title.string)
+        assert_in('<title>Reports', _get_response_body(response))
 
     def test_report_index_redirect(self):
         response = self.app.get('/reports', status=[301, 302])
