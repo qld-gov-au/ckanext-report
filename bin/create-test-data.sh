3aadb343b2d951ae7cc079e9fea9a4ec3acc7430
ThrawnCA <shell_layer-github@yahoo.com.au>
2022-08-30 08:16:26 +1000

    [QOL-9055] add Behave-based scenario testing

---
+++ bin/create-test-data.sh
@@ -0,0 +1,116 @@
+#!/usr/bin/env sh
+##
+# Create some example content for extension BDD tests.
+#
+set -e
+
+CKAN_ACTION_URL=${CKAN_SITE_URL}api/action
+CKAN_USER_NAME="${CKAN_USER_NAME:-admin}"
+CKAN_DISPLAY_NAME="${CKAN_DISPLAY_NAME:-Administrator}"
+CKAN_USER_EMAIL="${CKAN_USER_EMAIL:-admin@localhost}"
+
+. ${APP_DIR}/bin/activate
+
+add_user_if_needed () {
+    echo "Adding user '$2' ($1) with email address [$3]"
+    ckan_cli user show "$1" | grep "$1" || ckan_cli user add "$1"\
+        fullname="$2"\
+        email="$3"\
+        password="${4:-Password123!}"
+}
+
+add_user_if_needed "$CKAN_USER_NAME" "$CKAN_DISPLAY_NAME" "$CKAN_USER_EMAIL"
+ckan_cli sysadmin add "${CKAN_USER_NAME}"
+
+API_KEY=$(ckan_cli user show "${CKAN_USER_NAME}" | tr -d '\n' | sed -r 's/^(.*)apikey=(\S*)(.*)/\2/')
+if [ "$API_KEY" = "None" ]; then
+    echo "No API Key found on ${CKAN_USER_NAME}, generating API Token..."
+    API_KEY=$(ckan_cli user token add "${CKAN_USER_NAME}" test_setup |tail -1 | tr -d '[:space:]')
+fi
+
+##
+# BEGIN: Create a test organisation with test users for admin, editor and member
+#
+TEST_ORG_NAME=test-organisation
+TEST_ORG_TITLE="Test Organisation"
+
+echo "Creating test users for ${TEST_ORG_TITLE} Organisation:"
+
+add_user_if_needed ckan_user "CKAN User" ckan_user@localhost
+add_user_if_needed test_org_admin "Test Admin" test_org_admin@localhost
+add_user_if_needed test_org_editor "Test Editor" test_org_editor@localhost
+add_user_if_needed test_org_member "Test Member" test_org_member@localhost
+
+echo "Creating ${TEST_ORG_TITLE} organisation:"
+
+TEST_ORG=$( \
+    curl -LsH "Authorization: ${API_KEY}" \
+    --data '{"name": "'"${TEST_ORG_NAME}"'", "title": "'"${TEST_ORG_TITLE}"'"}' \
+    ${CKAN_ACTION_URL}/organization_create
+)
+
+TEST_ORG_ID=$(echo $TEST_ORG | $PYTHON ${APP_DIR}/bin/extract-id.py)
+
+echo "Assigning test users to '${TEST_ORG_TITLE}' organisation (${TEST_ORG_ID}):"
+
+curl -LsH "Authorization: ${API_KEY}" \
+    --data '{"id": "'"${TEST_ORG_ID}"'", "object": "test_org_admin", "object_type": "user", "capacity": "admin"}' \
+    ${CKAN_ACTION_URL}/member_create
+
+curl -LsH "Authorization: ${API_KEY}" \
+    --data '{"id": "'"${TEST_ORG_ID}"'", "object": "test_org_editor", "object_type": "user", "capacity": "editor"}' \
+    ${CKAN_ACTION_URL}/member_create
+
+curl -LsH "Authorization: ${API_KEY}" \
+    --data '{"id": "'"${TEST_ORG_ID}"'", "object": "test_org_member", "object_type": "user", "capacity": "member"}' \
+    ${CKAN_ACTION_URL}/member_create
+##
+# END.
+#
+
+##
+# BEGIN: Create a Reporting organisation with test users
+#
+
+REPORT_ORG_NAME=reporting
+REPORT_ORG_TITLE=Reporting
+
+echo "Creating test users for ${REPORT_ORG_TITLE} Organisation:"
+
+add_user_if_needed report_admin "Reporting Admin" report_admin@localhost
+add_user_if_needed report_editor "Reporting Editor" report_editor@localhost
+
+echo "Creating ${REPORT_ORG_TITLE} Organisation:"
+
+REPORT_ORG=$( \
+    curl -LsH "Authorization: ${API_KEY}" \
+    --data '{"name": "'"${REPORT_ORG_NAME}"'", "title": "'"${REPORT_ORG_TITLE}"'"}' \
+    ${CKAN_ACTION_URL}/organization_create
+)
+
+REPORT_ORG_ID=$(echo $REPORT_ORG | $PYTHON ${APP_DIR}/bin/extract-id.py)
+
+echo "Assigning test users to ${REPORT_ORG_TITLE} Organisation:"
+
+curl -LsH "Authorization: ${API_KEY}" \
+    --data '{"id": "'"${REPORT_ORG_ID}"'", "object": "report_admin", "object_type": "user", "capacity": "admin"}' \
+    ${CKAN_ACTION_URL}/member_create
+
+curl -LsH "Authorization: ${API_KEY}" \
+    --data '{"id": "'"${REPORT_ORG_ID}"'", "object": "report_editor", "object_type": "user", "capacity": "editor"}' \
+    ${CKAN_ACTION_URL}/member_create
+
+echo "Creating test dataset for reporting:"
+
+curl -LsH "Authorization: ${API_KEY}" \
+    --data '{"name": "reporting", "title": "Reporting dataset", "description": "Dataset for testing reports",
+"owner_org": "'"${REPORT_ORG_ID}"'", "update_frequency": "near-realtime", "author_email": "report_admin@localhost",
+"version": "1.0", "license_id": "cc-by-4", "data_driven_application": "NO", "security_classification": "PUBLIC",
+"notes": "test", "de_identified_data": "NO"}'\
+    ${CKAN_ACTION_URL}/package_create
+
+##
+# END.
+#
+
+. ${APP_DIR}/bin/deactivate
