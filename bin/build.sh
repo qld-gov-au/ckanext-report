01dae84a243f1ab166fa2e7779147b6cee6bb1e0
ThrawnCA <carl.antuar@smartservice.qld.gov.au>
2026-07-22 12:50:50 +1000

    [QOLDEV-2088] sync test config with other repos
    - Drop unnecessary 'drevops' container
    - Update test dependencies
    - Install 'wget' if test container doesn't have it
    - Split test logs into groups by container

--- bin/build.sh
+++ bin/build.sh
@@ -14,12 +14,18 @@ sed -i -e "s/##//" docker-compose.yml
 # Pull the latest images.
 ahoy pull
 
+PYTHON=python3
 PYTHON_VERSION=py3
-PYTHON="python3"
+
+SOLR_VERSION=9
+if [ "$CKAN_VERSION" = "2.9" ]; then
+    SOLR_VERSION=8
+fi
 
 sed "s|{CKAN_VERSION}|$CKAN_VERSION|g" .docker/Dockerfile-template.ckan \
     | sed "s|{PYTHON_VERSION}|$PYTHON_VERSION|g" \
     | sed "s|{PYTHON}|$PYTHON|g" \
     > .docker/Dockerfile.ckan
 
+export SOLR_VERSION
 ahoy build
